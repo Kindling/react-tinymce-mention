c06185e45845be935a2d7eb4a5bb6db648f44022
Christopher Pappas <damassi.pappas@gmail.com>
2015-07-28 17:28:05 -0700

    Update list

---
+++ src/mentions/components/UserList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class UserList {
+  render() {
+    return (
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+        <li>d</li>
+      </ul>
+    );
+  }
+}

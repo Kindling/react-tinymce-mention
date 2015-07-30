c0b05940e49cd30aa310c51b4d5314600def197b
Christopher Pappas <damassi.pappas@gmail.com>
2015-07-30 15:40:19 -0700

    Stubbed out tests and scripts

---
+++ test/actions/mentionActions.spec.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Testing Actions', () => {
+  it('should test', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ test/reducers/mentionReducer.spec.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Testing reducer', () => {
+  it('should test', () => {
+    expect(true).toEqual(true);
+  });
+});

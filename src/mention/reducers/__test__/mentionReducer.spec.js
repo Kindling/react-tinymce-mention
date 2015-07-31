3cd52b294e00bc44e5d5490c08515bfad9b10aa8
Christopher Pappas <damassi.pappas@gmail.com>
2015-07-31 14:39:22 -0700

    Tests now working

--- src/mention/reducers/__test__/mentionReducer.spec.js
+++ src/mention/reducers/__test__/mentionReducer.spec.js
@@ -1,5 +1,18 @@
-describe('mentionReducer', () => {
-  it('Should work', () => {
+import initializeRedux from '../../utils/initializeRedux';
+import mentionReducer from '../mentionReducer';
+
+describe('mentionReducer', function() {
+
+  var store;
+
+  beforeEach(function() {
+    store = initializeRedux({
+      mention: mentionReducer
+    });
+  });
+
+  it('Should work', function() {
+    console.log(store.getState())
     expect(true).toBe(true);
   });
 });

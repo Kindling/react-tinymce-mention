0355d9e722cc9af656fc9c6b4136c45fe0e5b6d9
Christopher Pappas <damassi.pappas@gmail.com>
2015-08-08 00:15:52 -0700

    CU

--- test/unit.webpack.loader.js
+++ test/unit.webpack.loader.js
@@ -1,15 +1,5 @@
-/**
- * Adaptor for running TDD-based unit tests.  Looks through all directories
- * executes files with `-test` appended to filename.
- *
- * @author Christopher Pappas <christopher.pappas@kindlingapp.com>
- * @since  1.1.2015
- */
-
 import 'babel/polyfill';
 
-// Utilize Webpack's dynamic require to fetch only relevant files
 var context = require.context('../src/', true, /\.spec\.(js|jsx)$/);
 
-// Require matches
 context.keys().forEach(context);

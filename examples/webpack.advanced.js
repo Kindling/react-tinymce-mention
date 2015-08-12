fdbc501d7252896ba2ba12ff0465ef16cd96f629
Christopher Pappas <damassi.pappas@gmail.com>
2015-08-11 17:59:19 -0700

    More restructuring of examples

--- examples/webpack.advanced.js
+++ examples/webpack.advanced.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/index'
+    './advanced/src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
@@ -24,7 +24,7 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'advanced/src')
     }]
   }
 };

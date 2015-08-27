58a1686c9724b60e950aede5579dc088f4a9578f
David Ashby <delta.mu.alpha@gmail.com>
2015-08-27 17:31:39 -0400

    some tests for the tinymce utility functions

---
+++ src/mention/utils/__tests__/tinyMCEUtils.spec.js
@@ -0,0 +1,29 @@
+import {
+  getLastChar,
+  getEditorContent,
+  collectMentionIds
+} from '../tinyMCEUtils';
+import initializeEditor from '../../__tests__/fixtures/initializeEditor';
+
+describe('tinyMCEUtils', () => {
+
+  beforeEach(function() {
+    var tinymce = initializeEditor();
+  });
+
+  xit('return the last character in a text area', () => {
+    tinymce.activeEditor.setContent('<p>lorem ipsum</p>');
+    expect(getLastChar(tinymce.activeEditor)).toEqual('m');
+  });
+
+  it('return the full contents of an editor', () => {
+    tinymce.activeEditor.setContent('<p>lorem ipsum</p>');
+    expect(getEditorContent(tinymce.activeEditor)).toEqual('lorem ipsum');
+    expect(getEditorContent(tinymce.activeEditor, 'raw')).toEqual('<p>lorem ipsum</p>');
+  });
+
+  it('return the ids of all mentions in an editor', () => {
+    tinymce.activeEditor.setContent('<p><a href="#" id="1" class="mention">mention</a> lorem ipsum</p>');
+    expect(collectMentionIds(tinymce.activeEditor, '.mention')).toEqual(['1']);
+  });
+});

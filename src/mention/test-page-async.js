8b3efcc4913deb63f82e12a472b3e1a3846d47f2
Christopher Pappas <damassi.pappas@gmail.com>
2015-09-04 18:49:23 -0700

    Sync test pages

--- src/mention/test-page-async.js
+++ src/mention/test-page-async.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TinyMCE from 'react-tinymce';
 import Mention from './Mention';
-import axios from 'axios';
 
 var plugins = [
   'autolink',
@@ -16,18 +15,25 @@ var plugins = [
 
 let initialContent = `Sed ut perspiciatis unde omnis
 iste natus error sit voluptatem accusantium doloremque
+
 laudantium, totam rem aperiam, eaque ipsa quae ab illo
 <br /><br />
 <br />
+
+
 inventore veritatis et quasi architecto beatae
 vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
 quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
 dolores eos qui ratione voluptatem sequi nesciunt.
 <br />
 <br />
+
 Neque porro quisquam est, qui dolorem ipsum
+
 <br />
 <br />
+
+
 quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
 tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
 minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi
@@ -35,111 +41,83 @@ ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui 
 voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
 fugiat quo voluptas nulla pariatur?`;
 
-initialContent = '';
+// initialContent = '';
 
-renderMentions();
-renderEditor();
+React.render(
+  <div>
+    <TinyMCE
+      content={initialContent}
+      config={{
+        browser_spellcheck: true,
+        document_base_url: window.location.origin + '/',
 
-function renderEditor() {
-  React.render(
-    <div>
-      <TinyMCE
-        content={initialContent}
-        config={{
-          browser_spellcheck: true,
-          document_base_url: window.location.origin + '/',
+        // FIXME
+        cleanup: false,
+        entity_encoding: 'named',
+        entities: '160,nbsp',
 
-          // FIXME
-          cleanup: false,
-          entity_encoding: 'named',
-          entities: '160,nbsp',
+        extended_valid_elements: 'blockquote[dir|style|cite|class|dir<ltr?rtl],iframe[src|frameborder|style|scrolling|class|width|height|name|align],pre',
+        ie7_compat: false,
+        image_description: false,
+        image_dimensions: false,
+        media_alt_source: false,
+        media_poster: false,
+        media_dimensions: false,
+        menubar: false,
+        plugins: plugins.join(','),
 
-          extended_valid_elements: 'blockquote[dir|style|cite|class|dir<ltr?rtl],iframe[src|frameborder|style|scrolling|class|width|height|name|align],pre',
-          ie7_compat: false,
-          image_description: false,
-          image_dimensions: false,
-          media_alt_source: false,
-          media_poster: false,
-          media_dimensions: false,
-          menubar: false,
-          plugins: plugins.join(','),
+        // We always want the _full URL_ - not the relative path.
+        relative_urls: false,
+        remove_script_host: false,
+        skin: 'kindling',
+        statusbar: false,
 
-          // We always want the _full URL_ - not the relative path.
-          relative_urls: false,
-          remove_script_host: false,
-          skin: 'kindling',
-          statusbar: false,
-
-          // Suppress the target option for links.
-          target_list: false,
-          theme: 'kindling',
-          toolbar: 'bold italic underline strikethrough | bullist numlist blockquote | link unlink | image media | removeformat code'
-        }}
-      />
-      <button onClick={renderMentions}>reload</button>
-    </div>
-  , document.getElementById('editor'));
-}
-
-function renderMentions() {
-  let node = document.getElementById('mentions');
-
-  if (React.unmountComponentAtNode(node)) {
-    console.log('reload');
-  }
-
-  React.render(
-    <div>
-      <Mention
-        delimiter={'@'}
-        showDebugger={true}
-        _dataSource={axios.get('/examples/shared/api/complex.json')}
-        asyncDataSource={() => {
-          return new Promise((resolve) => {
-            axios.get('/examples/shared/api/complex.json').then(response => {
-              resolve(transformDataSource(response));
-            });
+        // Suppress the target option for links.
+        target_list: false,
+        theme: 'kindling',
+        toolbar: 'bold italic underline strikethrough | bullist numlist blockquote | link unlink | image media | removeformat code'
+      }}
+    />
+    <Mention
+      showDebugger={true}
+      _dataSource={axios.get('/examples/shared/api/complex.json')}
+      asyncDataSource={() => {
+        return new Promise((resolve) => {
+          axios.get('/examples/shared/api/complex.json').then(response => {
+            resolve(transformDataSource(response));
           });
-        }}
-
-        transformFn={transformDataSource}
-
-        customRTEMention={(props) => {
-          const { tinymceId, displayLabel } = props;
-          return (
-            <span>
-              <a href='#' id={tinymceId} className='tinymce-mention'>
-                @{displayLabel}
-              </a>
-              &nbsp;
-            </span>
-          );
-        }}
-
-        onAdd={({ mentions, changed }) => {
-          console.log('ADDED: ', mentions, 'changed: ', changed);
-        }}
-        onRemove={({ mentions, changed }) => {
-          console.log('REMOVED: ', mentions, 'changed: ', changed);
-        }}
-
-      />
-    </div>
-  , node);
-}
+        });
+      }}
+      transformFn={transformDataSource}
+      customRTEMention={(props) => {
+        const { tinymceId, displayLabel } = props;
+        return (
+          <span>
+            <a href='#' id={tinymceId} className='tinymce-mention'>
+              @{displayLabel}
+            </a>
+            &nbsp;
+          </span>
+        );
+      }}
+      onAdd={({ mentions, changed }) => {
+        console.log('ADDED: ', mentions, 'changed: ', changed);
+      }}
+      onRemove={({ mentions, changed }) => {
+        console.log('REMOVED: ', mentions, 'changed: ', changed);
+      }}
+    />
+  </div>
+, document.getElementById('mentions'));
 
 function transformDataSource(dataSource) {
-  if (dataSource && dataSource.data) {
-    const complexDataSource = dataSource.data.map(result => {
-      const { fullName } = result;
-      return {
-        searchKey: fullName,
-        displayLabel: fullName
-      };
-    });
+  const complexDataSource = dataSource.map(result => {
+    const { fullName } = result;
+    return {
+      searchKey: fullName,
+      displayLabel: fullName
+    };
+  });
 
-    return complexDataSource;
-  } else {
-    return [];
-  }
+  return complexDataSource;
 }
--- src/mention/test-page-promise.js
+++ src/mention/test-page-promise.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import TinyMCE from 'react-tinymce';
 import Mention from './Mention';
-import CustomList from './components/CustomList';
-import EditorMention from './components/EditorMention';
-import complexDataSource from './test-data-source';
-import axios from 'axios';
 
 var plugins = [
   'autolink',
@@ -19,18 +15,25 @@ var plugins = [
 
 let initialContent = `Sed ut perspiciatis unde omnis
 iste natus error sit voluptatem accusantium doloremque
+
 laudantium, totam rem aperiam, eaque ipsa quae ab illo
 <br /><br />
 <br />
+
+
 inventore veritatis et quasi architecto beatae
 vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
 quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
 dolores eos qui ratione voluptatem sequi nesciunt.
 <br />
 <br />
+
 Neque porro quisquam est, qui dolorem ipsum
+
 <br />
 <br />
+
+
 quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
 tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
 minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi
@@ -40,97 +43,73 @@ fugiat quo voluptas nulla pariatur?`;
 
 initialContent = '';
 
-renderMentions();
-renderEditor();
+React.render(
+  <div>
+    <TinyMCE
+      content={initialContent}
+      config={{
+        browser_spellcheck: true,
+        document_base_url: window.location.origin + '/',
 
-function renderEditor() {
-  React.render(
-    <div>
-      <TinyMCE
-        content={initialContent}
-        config={{
-          browser_spellcheck: true,
-          document_base_url: window.location.origin + '/',
+        // FIXME
+        cleanup: false,
+        entity_encoding: 'named',
+        entities: '160,nbsp',
 
-          // FIXME
-          cleanup: false,
-          entity_encoding: 'named',
-          entities: '160,nbsp',
+        extended_valid_elements: 'blockquote[dir|style|cite|class|dir<ltr?rtl],iframe[src|frameborder|style|scrolling|class|width|height|name|align],pre',
+        ie7_compat: false,
+        image_description: false,
+        image_dimensions: false,
+        media_alt_source: false,
+        media_poster: false,
+        media_dimensions: false,
+        menubar: false,
+        plugins: plugins.join(','),
 
-          extended_valid_elements: 'blockquote[dir|style|cite|class|dir<ltr?rtl],iframe[src|frameborder|style|scrolling|class|width|height|name|align],pre',
-          ie7_compat: false,
-          image_description: false,
-          image_dimensions: false,
-          media_alt_source: false,
-          media_poster: false,
-          media_dimensions: false,
-          menubar: false,
-          plugins: plugins.join(','),
+        // We always want the _full URL_ - not the relative path.
+        relative_urls: false,
+        remove_script_host: false,
+        skin: 'kindling',
+        statusbar: false,
 
-          // We always want the _full URL_ - not the relative path.
-          relative_urls: false,
-          remove_script_host: false,
-          skin: 'kindling',
-          statusbar: false,
+        // Suppress the target option for links.
+        target_list: false,
+        theme: 'kindling',
+        toolbar: 'bold italic underline strikethrough | bullist numlist blockquote | link unlink | image media | removeformat code'
+      }}
+    />
+    <Mention
+      dataSource={axios.get('/examples/shared/api/complex.json')}
+      delimiter={'@'}
+      showDebugger={true}
+      customRTEMention={(props) => {
+        const { tinymceId, displayLabel } = props;
+        return (
+          <span>
+            <a href='#' id={tinymceId} className='tinymce-mention'>
+              @{displayLabel}
+            </a>
+            &nbsp;
+          </span>
+        );
+      }}
+      transformFn={dataSource => {
+        const complexDataSource = dataSource.data.map(result => {
+          const { fullName } = result;
+          return {
+            searchKey: fullName,
+            displayLabel: fullName
+          };
+        });
 
-          // Suppress the target option for links.
-          target_list: false,
-          theme: 'kindling',
-          toolbar: 'bold italic underline strikethrough | bullist numlist blockquote | link unlink | image media | removeformat code'
-        }}
-      />
-      <button onClick={renderMentions}>reload</button>
-    </div>
-  , document.getElementById('editor'));
-}
-
-function renderMentions() {
-  let node = document.getElementById('mentions');
-
-  if (React.unmountComponentAtNode(node)) {
-    console.log('reload');
-  }
-
-  React.render(
-    <div>
-      <Mention
-        dataSource={axios.get('/examples/shared/api/complex.json')}
-        delimiter={'@'}
-        showDebugger={true}
-
-        customRTEMention={(props) => {
-          const { tinymceId, displayLabel } = props;
-          return (
-            <span>
-              <a href='#' id={tinymceId} className='tinymce-mention'>
-                @{displayLabel}
-              </a>
-              &nbsp;
-            </span>
-          );
-        }}
-
-        transformFn={dataSource => {
-          const complexDataSource = dataSource.data.map(result => {
-            const { fullName } = result;
-            return {
-              searchKey: fullName,
-              displayLabel: fullName
-            };
-          });
-
-          return complexDataSource;
-        }}
-
-        onAdd={({ mentions, changed }) => {
-          // console.log('ADDED: ', mentions, 'changed: ', changed);
-        }}
-
-        onRemove={({ mentions, changed }) => {
-          // console.log('REMOVED: ', mentions, 'changed: ', changed);
-        }}
-
-      />
-    </div>
-  , node);
-}
+        return complexDataSource;
+      }}
+      onAdd={({ mentions, changed }) => {
+        console.log('ADDED: ', mentions, 'changed: ', changed);
+      }}
+      onRemove={({ mentions, changed }) => {
+        console.log('REMOVED: ', mentions, 'changed: ', changed);
+      }}
+    />
+  </div>
+, document.getElementById('mentions'));

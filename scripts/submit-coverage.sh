ba86e33c2c49c607552f791204f25af5d6ecc640
David Ashby <delta.mu.alpha@gmail.com>
2015-09-14 14:15:09 -0400

    add a guard for no code climate token

--- scripts/submit-coverage.sh
+++ scripts/submit-coverage.sh
@@ -1,2 +1,6 @@
-LCOV=`find coverage/ -name lcov.info`
-node_modules/.bin/codeclimate-test-reporter < $LCOV;
+if [[ ! "$CODECLIMATE_REPO_TOKEN" = "" ]]; then
+  LCOV=`find coverage/ -name lcov.info`
+  node_modules/.bin/codeclimate-test-reporter < $LCOV;
+else
+  echo "no \$CODECLIMATE_REPO_TOKEN set, skipping report...";
+fi
